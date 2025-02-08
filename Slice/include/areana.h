a909dc2390d438cf999cfa2362335cc85b160bcf
Andrea Passini <andreapa98@gmail.com>
2025-02-08 23:28:01 +0100

    Testing memory leak with linked chunk of arenas

--- Slice/include/areana.h
+++ Slice/include/areana.h
@@ -1,44 +1,91 @@
 #include <assert.h>
 #include <iostream>
 
+// Just for test
+static int FREE_TIMES = 0;
+
 struct Arena {
 	size_t capacity = 0;
 	size_t size = 0;
 	uint8_t* data = nullptr;
+	Arena* nextArena = nullptr;
 };
 
 void InitArena(Arena* const arena, const size_t capacity) {
 	arena->data = (uint8_t*)malloc(sizeof(arena->data) * capacity);
-	if (arena->data) {
-		arena->capacity = capacity;
-		arena->size = 0;
-	}
+	arena->capacity = capacity;
+	arena->size = 0;
+	arena->nextArena = nullptr;
 }
 
 template<typename T>
 T* Allocate(Arena* const arena, const size_t size) {
 	T* outPtr = nullptr;
 
-	// Arena doesnt have extra size
-	//if (!(arena->size + size <= arena->capacity)) { return outPtr; }
-	assert ((arena->size + size <= arena->capacity));
+	Arena* currentArena = arena;
 
-	outPtr = (T*)(arena->data + ((sizeof(arena->data) * arena->size)));
+	// Arena doesn't have extra size
+	assert(currentArena);
+	while (!(currentArena->size + size <= currentArena->capacity)) {
+		if (!currentArena->nextArena) {
+			size_t NextCapacity = currentArena->capacity >= size ? currentArena->capacity : size;
+			currentArena->nextArena = (Arena*)malloc(sizeof(Arena));
+			InitArena(currentArena->nextArena, NextCapacity);
+			std::cout << "Init NextArena w/ cap = " << NextCapacity << std::endl;
+		}
+		
+		currentArena = currentArena->nextArena;
+	}
 
-	arena->size += size;
+	outPtr = (T*)(currentArena->data + ((sizeof(currentArena->data) * currentArena->size)));
+
+	currentArena->size += size;
 
 	return outPtr;
 }
 
 // Reset the size of the arena
 void Reset(Arena* const arena) {
+	if (arena->nextArena) {
+		Reset(arena->nextArena);
+	}
 	arena->size = 0;
 }
 
-void Free(Arena* const arena) {
-	free(arena->data);
-	arena->capacity = 0;
-	arena->size = 0;
+void Free(Arena* arena) {
+
+	if (arena && arena->data) {
+		free(arena->data);
+		arena->size = 0;
+		arena->capacity = 0;
+		arena->data = nullptr;
+
+		if (arena->nextArena) {
+			Free(arena->nextArena);
+		}
+
+		// dont free the last one, this should be handled by the user
+		free(arena);
+		arena = nullptr;
+	}
+
+	//Arena* currentArena = nullptr;
+	//currentArena = arena;
+
+	//while (currentArena && currentArena->data) {
+	//	free(currentArena->data);
+	//	currentArena->size = 0;
+	//	currentArena->capacity = 0;
+	//	currentArena->data = nullptr;
+
+	//	Arena* nextArena = currentArena->nextArena;
+
+	//	free(currentArena);
+
+	//	if (nextArena) {
+	//		currentArena = nextArena;
+	//	}
+	//}
 }
 
 void PrintArena(const Arena* const arena)
@@ -49,4 +96,10 @@ void PrintArena(const Arena* const arena)
 		"\n Arena.capacity = " << arena->capacity <<
 		"\n Arena.data = " << (void*)arena->data << 
 		"\n Arena.NextData = " << (void*)NextData << std::endl;
+
+	if (arena->nextArena) {
+		PrintArena(arena->nextArena);
+	}
+
+	std::cout << "Free times" << FREE_TIMES << std::endl;
 }
--- Slice/src/main.cpp
+++ Slice/src/main.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
-#include <assert.h>
 
 #include "../include/areana.h"
 
 int main()
 {
-	Arena arena;
-	InitArena(&arena, 1'000);
+	Arena*  arena = (Arena*)malloc(sizeof(Arena));
+	InitArena(arena, 1'000);
 
-	uint8_t* ptr1 = Allocate<uint8_t>(&arena, 123);
-	PrintArena(&arena);
-	uint8_t* ptr2 = Allocate<uint8_t>(&arena, 123);
-	PrintArena(&arena);
-	uint8_t* ptr3 = Allocate<uint8_t>(&arena, 123);
-	PrintArena(&arena);
+	uint8_t* ptr1 = Allocate<uint8_t>(arena, 1200);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
 
-	Free(&arena);
-	PrintArena(&arena);
+	uint8_t* ptr2 = Allocate<uint8_t>(arena, 1000);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
 
-	std::cout << "Hello" << std::endl;
+	uint8_t* ptr3 = Allocate<uint8_t>(arena, 1000);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
+
+	uint8_t* ptr4 = Allocate<uint8_t>(arena, 1000);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
+
+	uint8_t* ptr5 = Allocate<uint8_t>(arena, 1000);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
+
+	uint8_t* ptr6 = Allocate<uint8_t>(arena, 2000);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
+
+	std::cout << "\n Resetting" << std::endl;
+	Reset(arena);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
+
+	std::cout << "\n Freeing" << std::endl;
+	Free(arena);
+	PrintArena(arena);
+	std::cout << "\n" << std::endl;
+
+	ptr1 = nullptr;
+	ptr2 = nullptr;
+	ptr3 = nullptr;
+	ptr4 = nullptr;
+	ptr5 = nullptr;
+	ptr6 = nullptr;
+
+	std::cout << "The end" << std::endl;
 
 	return 0;
 }
