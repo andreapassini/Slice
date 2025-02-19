81c66993c9c140bc7f6e178a17d843920de9d8c7
Andrea Passini <andreapa98@gmail.com>
2025-02-19 18:58:49 +0100

    Update arena.h

--- Slice/include/arena.h
+++ Slice/include/arena.h
@@ -1,14 +1,18 @@
 #include <assert.h>
 #include <iostream>
 
-struct Arena {
+#define RESIZE_MULTIPLIER 1.5f
+
+struct Arena
+{
 	size_t capacity = 0;
 	size_t size = 0;
 	uint8_t* data = nullptr;
 	Arena* nextArena = nullptr;
 };
 
-void InitArena(Arena* const arena, const size_t capacity) {
+void InitArena(Arena* const arena, const size_t capacity)
+{
 	arena->data = (uint8_t*)malloc(sizeof(arena->data) * capacity);
 	arena->capacity = capacity;
 	arena->size = 0;
@@ -16,21 +20,24 @@ void InitArena(Arena* const arena, const size_t capacity) {
 }
 
 template<typename T>
-T* Allocate(Arena* const arena, const size_t size) {
+T* Allocate(Arena* const arena, const size_t size)
+{
 	T* outPtr = nullptr;
 
 	Arena* currentArena = arena;
 
 	// Arena doesn't have extra size
 	assert(currentArena);
-	while (!(currentArena->size + size <= currentArena->capacity)) {
-		if (!currentArena->nextArena) {
-			size_t NextCapacity = currentArena->capacity >= size ? currentArena->capacity : size;
+	while (!(currentArena->size + size <= currentArena->capacity))
+	{
+		if (!currentArena->nextArena)
+		{
+			size_t NextCapacity = currentArena->capacity >= size ? currentArena->capacity : size * RESIZE_MULTIPLIER;
 			currentArena->nextArena = (Arena*)malloc(sizeof(Arena));
 			InitArena(currentArena->nextArena, NextCapacity);
 			std::cout << "Init NextArena w/ cap = " << NextCapacity << std::endl;
 		}
-		
+
 		currentArena = currentArena->nextArena;
 	}
 
@@ -42,46 +49,44 @@ T* Allocate(Arena* const arena, const size_t size) {
 }
 
 // Reset the size of the arena
-void Reset(Arena* const arena) {
-	if (arena->nextArena) {
+void Reset(Arena* const arena)
+{
+	if (arena->nextArena)
+	{
 		Reset(arena->nextArena);
 	}
 	arena->size = 0;
 }
 
-void Free(Arena* arena) {
-	if (!arena)
-	{
-		return;
-	}
-
-	if (arena->data)
+void Free(Arena* arena)
+{
+	if (arena && arena->data)
 	{
 		free(arena->data);
 		arena->data = nullptr;
-	}
-	arena->size = 0;
-	arena->capacity = 0;
+		arena->size = 0;
+		arena->capacity = 0;
 
-	Arena* currentArena = nullptr;
-	currentArena = arena->nextArena;
-	while (currentArena)
-	{
-		if (currentArena->data)
+		Arena* currentArena = nullptr;
+		currentArena = arena->nextArena;
+		while (currentArena && currentArena->data)
 		{
 			free(currentArena->data);
 			currentArena->data = nullptr;
+			currentArena->size = 0;
+			currentArena->capacity = 0;
+
+			Arena* nextArena = nullptr;
+			nextArena = currentArena->nextArena;
+			free(currentArena);
+			currentArena = nextArena;
 		}
-		currentArena->size = 0;
-		currentArena->capacity = 0;
 
-		Arena* nextArena = nullptr;
-		nextArena = currentArena->nextArena;
-		free(currentArena);
-		currentArena = nextArena;
+		// Maybe the last should be handled by the user
+		// it could be stack allocated
+		//free(arena);
+		//arena = nullptr;
 	}
-
-	arena->nextArena = nullptr;
 }
 
 void PrintArena(const Arena* const arena)
@@ -90,10 +95,13 @@ void PrintArena(const Arena* const arena)
 
 	std::cout << "Arena.size = " << arena->size <<
 		"\n Arena.capacity = " << arena->capacity <<
-		"\n Arena.data = " << (void*)arena->data << 
+		"\n Arena.data = " << (void*)arena->data <<
 		"\n Arena.NextData = " << (void*)NextData << std::endl;
 
-	if (arena->nextArena) {
+	if (arena->nextArena)
+	{
 		PrintArena(arena->nextArena);
 	}
+
+	std::cout << "Free times" << FREE_TIMES << std::endl;
 }
